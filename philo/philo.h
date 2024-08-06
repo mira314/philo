ef414276d3b530935e171824f72f59bb03093c30
Valisoa Mirado Narindra Randrianoelisoa <vrandria@c1r11s5.42antananarivo.mg>
2024-08-06 14:28:29 +0300

    ok

---
+++ philo/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrandria <vrandria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 16:47:32 by vrandria          #+#    #+#             */
+/*   Updated: 2024/08/06 14:23:32 by vrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <stdlib.h>   
+# include <unistd.h>
+#include <stdbool.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdint.h>
+# include <pthread.h>
+
+typedef struct s_data t_data;
+
+
+#endif
